9f883c69842b7df1a626d0707bcd4fd9dc46afb6
ahmed elsayed <a7md1elsayed@gmail.com>
2026-06-12 02:08:19 +0300

    add pro

--- src/app/component/projects/projects.component.ts
+++ src/app/component/projects/projects.component.ts
@@ -8,36 +8,52 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
+  projectsToShow = 3;
+
   projects = [
     {
       title: 'Menu Drinks',
       description:
         'A responsive and modern drinks menu showcasing a variety of beverages with appealing visuals and smooth user interactions. Built using Angular and Bootstrap for an elegant and dynamic user experience.',
-
-      image: '/project/1.png',
+      image: '/project/coffe.png',
       technologies: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Bootstrap'],
-      liveUrl: '#',
-      githubUrl: '#',
+      liveUrl: 'https://coffee-sepia-mu.vercel.app/home',
+      githubUrl: 'https://github.com/A7med953/coffee',
     },
     {
-      title: 'Portfolio',
+      title: 'HealthCarePlus',
+      image: '/project/HealthCarePlus.png',
       description:
-        'A personal portfolio website showcasing projects, skills, and experience with a modern and responsive design. Built with Angular and Bootstrap to ensure smooth navigation and performance.',
-      image: '/project/2.png',
-
-      technologies: ['HTML', 'CSS', 'Bootstrap', 'JavaScript'],
-      liveUrl: '#',
-      githubUrl: '#',
+        'A role-based healthcare platform for patients, doctors, and pharmacists. The system manages patient medical history, supports record tracking, and improves coordination between healthcare providers for better clinical workflow.',
+      technologies: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Bootstrap'],
+      liveUrl: 'https://team-depi-delta.vercel.app/',
+      githubUrl: 'http://github.com/A7med953/team-depi-',
     },
     {
-      title: 'Menu Restaurant',
+      title: 'Candle-store',
+      image: '/project/Candle-store.png',
       description:
-        'An interactive restaurant menu website showcasing dishes with images, prices, and categories. Built with Angular and Bootstrap for a smooth, responsive user experience.',
+  'A modern e-commerce platform for showcasing and selling handmade candles. The application provides a smooth user experience with a responsive layout, product display system, and clean UI designed to enhance online shopping engagement.',
+  technologies: [ 'JavaScript', 'HTML', 'CSS'],
+  liveUrl: 'https://a7med953.github.io/Candle-store/',
+  githubUrl: 'https://github.com/A7med953/Candle-store',
+    },
+    {
+      title: 'Bondi',
+      image: '/project/bondi.png',
 
-      image: '/project/3.png',
-      technologies: ['HTML', 'CSS', 'Bootstrap', 'JavaScript'],
-      liveUrl: '#',
-      githubUrl: '#',
+description:
+  'A modern personal portfolio website built to present projects, technical skills, and professional experience. It features a responsive layout, project showcase section, and clean UI to effectively demonstrate development capabilities.',
+  technologies: [ 'JavaScript', 'HTML', 'CSS','Bootstrap'],
+  liveUrl: 'https://a7med953.github.io/bondi/',
+  githubUrl: 'https://github.com/A7med953/bondi',
     },
   ];
+
+  showMoreProjects() {
+    this.projectsToShow += 3;
+  }
+  showLessProjects() {
+    this.projectsToShow = Math.max(3, this.projectsToShow - 3);
+  }
 }
